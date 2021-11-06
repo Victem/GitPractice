5dd5171d587f70e303c7c0696cbefe0c0c4b506e
Andrew Lobov <lobzan@mail.ru>
2021-11-06 10:30:24 +0300

    Remove console output

--- GitPractice/Program.cs
+++ GitPractice/Program.cs
@@ -6,8 +6,7 @@ namespace GitPractice
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!!!");
-            Console.WriteLine("Welcome to the club buddy!");
+
         }
     }
 }
